e7e8a88af0e63474fe048d81b880f2c35286f95d
Brett L. Schuchert <schuchert@yahoo.com>
2012-02-28 16:57:19 -0600

    Updated to include intellij project generation and put skeletal test file for presentation. Added intellij project files to .gitignore.

---
+++ WelcExamples/src/test/java/com/schuchert/welc/CurrencyConversionShould.java
@@ -0,0 +1,19 @@
+package com.schuchert.welc;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.math.BigDecimal;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import mockit.NonStrictExpectations;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.junit.Test;
+
+public class CurrencyConversionShould {
+}
